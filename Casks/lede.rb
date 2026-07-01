317e1d742967394ec6a4846a06009a49cd6fb566
Matan Gottlieb <matan.gottlieb@wisor.ai>
2026-07-01 10:20:59 +0300

    feat: add Lede cask v0.1.1

---
+++ Casks/lede.rb
@@ -0,0 +1,17 @@
+cask "lede" do
+  version "0.1.1"
+  sha256 "ea117a50fb9d79b587456a23333946ed9fe6d78abac324cc33fb0db7af7de687"
+
+  url "https://github.com/lede-md/lede/releases/download/v#{version}/Lede_#{version}_aarch64.dmg"
+  name "Lede"
+  desc "Fast, native Markdown editor"
+  homepage "https://github.com/lede-md/lede"
+
+  app "Lede.app"
+
+  caveats <<~EOS
+    Lede is not notarized. On first launch, right-click Lede.app and choose Open,
+    or run:
+      xattr -dr com.apple.quarantine "#{appdir}/Lede.app"
+  EOS
+end
